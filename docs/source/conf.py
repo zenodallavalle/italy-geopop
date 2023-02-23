b21bf3522c514fdee801d141566fb145753505c3
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-02-23 16:43:18 +0100

    adapted width

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,3 +66,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_js_files = ['js/custom.js']
+html_css_files = ['css/custom.css']
